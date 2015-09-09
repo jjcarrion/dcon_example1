19b883302f8dd3caf19ee827fddc62e9cac6eac2
Jose Jiménez <jose.jimenez@wunderkraut.com>
2015-09-09 19:12:54 +0200

    Added theme function

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # DrupalCon 2015
 ##Introduction to Drupal Custom Modules
+
+Theme function for the output.
---
+++ templates/dcon-example1-show-message.tpl.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * @file
+ * dcon-example1-show-message.tpl.php
+ */
+?>
+
+<h2><?php print $message ?></h2>
